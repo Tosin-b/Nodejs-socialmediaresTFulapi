be73c2e2206036368510e48f12afbfc14dec9db6
Tosin-b <tosynbiala@outlook.com>
2022-02-22 22:48:03 +0000

    added the update posts requests

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,34 @@
 const router = require("express").Router();
-
+const { response } = require("express");
+const Post = require("../models/Post")
 
 //create psot
+router.post("/", async (req,res)=>{
+    const newPost = new Post(req.body);
+    try{
+        const savedPost = await newPost.save()
+        res.status(200).json(savedPost)
 
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 //update post
+
+router.put("/:id",async(req, res)=>{
+    try{
+        const post = await Post.findById(req.params.id)
+        if(post.userId === req.body.userId){
+            await post.updateOne({$set: req.body})
+            res.status(200).json("Updated")
+        }else{
+            res.status(500).res("you can only update your own posts")
+        }
+
+    }catch(err){
+        res.status(500).res(err)
+    }
+} )
 //delete a post
 //like a posts
 //get a post 
--- routes/users.js
+++ routes/users.js
@@ -42,7 +42,7 @@ router.delete("/:id", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
-    const {password, updatedAt, ...others} = user._doc
+    const { password, updatedAt, ...others } = user._doc;
     res.status(200).send(others);
   } catch (err) {
     return res.status(500).send(err);
@@ -53,13 +53,13 @@ router.put("/:id/follow", async (req, res) => {
   if (req.body.userId !== req.params.id) {
     try {
       const user = await User.findById(req.params.id);
-      const currentUser = await User.findById(req.body.userId)
-      if(!user.followers.includes(req.body.userId)){
-          await user.updateOne({$push: {followers: req.body.userId}})
-          await currentUser.updateOne({$push: {following: req.params.id}})
-          res.status(200).json("User has benn followed")
-      }else{
-        res.status(403).send("you already follow this User")
+      const currentUser = await User.findById(req.body.userId);
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { following: req.params.id } });
+        res.status(200).json("User has benn followed");
+      } else {
+        res.status(403).send("you already follow this User");
       }
     } catch (err) {
       return res.status(500).send(err);
@@ -73,13 +73,13 @@ router.put("/:id/unfollow", async (req, res) => {
   if (req.body.userId !== req.params.id) {
     try {
       const user = await User.findById(req.params.id);
-      const currentUser = await User.findById(req.body.userId)
-      if(user.followers.includes(req.body.userId)){
-          await user.updateOne({$pull: {followers: req.body.userId}})
-          await currentUser.updateOne({$pull: {following: req.params.id}})
-          res.status(200).json("you dont follow this user")
-      }else{
-        res.status(403).send("you already follow this User")
+      const currentUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pull: { following: req.params.id } });
+        res.status(200).json("you dont follow this user");
+      } else {
+        res.status(403).send("you already follow this User");
       }
     } catch (err) {
       return res.status(500).send(err);
